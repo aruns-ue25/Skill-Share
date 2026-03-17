e91a5cccf6dcd988668c0820a2a6044a74f1f3d7
Arun <sugarthan.arun@gmail.com>
2026-03-17 22:39:47 +0530

    Add exception handling for profile errors

---
+++ Skill-Share/src/main/java/com/skillshare/skillshare/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.skillshare.skillshare.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String handleMaxSizeException(MaxUploadSizeExceededException exc, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("pictureError", "File is too large! Maximum allowed size is 5MB.");
+        return "redirect:/profile/edit";
+    }
+}
